8fa57d3213ac13ff9e69d4a06897845f3f99312b
Matteo <matteop18@gmail.com>
2020-03-10 16:35:17 +0100

    aggiunto caricamento

--- classes/Lavatrice.php
+++ classes/Lavatrice.php
@@ -21,5 +21,10 @@
   $lavasciuga = new Lavatrice('Samsung', '122 kWh/annum', 599, 'nero', '59db', 'A++');
   var_dump($lavasciuga);
 
+  $lavasciuga2 = new Lavatrice('Bosch', '150 kWh/annum', 239, 'blue', '79db', 'A+');
+  
+  $lavasciuga2->caricamento = 'Frontale';
+  var_dump($lavasciuga2);
+
 
  ?>

3f575fa2a53a0c49454b4acf3ce6e39fd349bfae
Matteo <matteop18@gmail.com>
2020-03-10 15:26:31 +0100

    prove

--- classes/Elettrodomestici.php
+++ classes/Elettrodomestici.php
@@ -14,13 +14,17 @@
       $this->colore = $colore;
     }
 
-    public function getPrice()
-    {
-      return $this->prezzo;
-    }
+    // public function getPrice()
+    // {
+    //   return $this->prezzo;
+    // }
   }
 
 
   $lavatrice = new Elettrodomestici ('Samsung', '122 kWh/annum', 599, 'nero');
   var_dump($lavatrice);
+  $frigorifero = new Elettrodomestici ('Bosch','365 kWh/annum', 750, 'argento');
+  var_dump($frigorifero);
+  $lavastoviglie = new Elettrodomestici ('Whirlpool','265 kWh/annum', 330, 'bianco');
+  var_dump($lavastoviglie);
 ?>
